335fdd9e367fe4d4d59997b2b42c8047893862a5
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-18 23:24:45 +0000

    Added lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :courses, only: [:index, :show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
