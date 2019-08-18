3db79b0346c391d74e88f484d4d8f5e94eda7c37
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-18 18:04:11 +0000

    Added form for course creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#index"
   resources :users, only: :show
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
