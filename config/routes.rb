bbf79633d9e373190b7619804caab69484041815
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-18 21:40:09 +0000

    Created Sections

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
 end
