020af55d4277af5bb1ca974810e2ec0500dc2ebb
Carrie Crow <carrie.g.crow@gmail.com>
2019-08-18 18:23:34 +0000

    Created course index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#index"
   resources :users, only: :show
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
