8d58d030f9f2301ced1ec7d6113e35615a00786b
Carrie Crow <carrie.g.crow@gmail.com>
2019-09-07 18:15:25 +0000

    Revised Course photos

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fill: [800, 350]
+  process resize_to_limit: [1000, 550]
   #
   # def scale(width, height)
   #   # do something
