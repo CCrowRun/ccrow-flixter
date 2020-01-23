fd0b73b728b98e66632cd36d6dddddc189a26397
Carrie <carrie.g.crow@gmail.com>
2020-01-23 18:20:35 +0000

    Confirmed premium courses return false for course#free?

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe Course, type: :model do
   let(:free_course) { Course.new(cost: 0) }
   let(:premium_course) { Course.new(cost: 99) }
+
   describe "course#free?" do
     it "returns true if a course has a cost value of zero" do
       expect(free_course.free?).to eq true
     end
-
+    it "returns false if a course has a cost value above zero" do
+      expect(premium_course.free?).to eq false
+    end
   end
 
   describe "it determines if a course is premium"
