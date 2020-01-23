a339c5768c5c8f8a7f03f204bb347e879e84c7e4
Carrie <carrie.g.crow@gmail.com>
2020-01-23 18:11:17 +0000

    Removed previous test folder

--- test/controllers/instructor/sections_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Instructor::SectionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/lessons_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LessonsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
