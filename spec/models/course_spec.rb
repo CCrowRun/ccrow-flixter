16a8e98ee3102a026580a19dd728983ea42d5acb
Carrie <carrie.g.crow@gmail.com>
2020-01-23 18:18:56 +0000

    Implemented course#free? for a course with a cost of 0

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  describe "it determines if a course is free"
+  let(:free_course) { Course.new(cost: 0) }
+  let(:premium_course) { Course.new(cost: 99) }
+  describe "course#free?" do
+    it "returns true if a course has a cost value of zero" do
+      expect(free_course.free?).to eq true
+    end
+
+  end
 
   describe "it determines if a course is premium"
 end
