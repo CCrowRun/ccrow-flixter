450ad9e76f41c58e4c17cfd1385927189e3bf154
Carrie <carrie.g.crow@gmail.com>
2020-01-29 20:29:03 +0000

    Created shell tests for courses controller

---
+++ spec/controllers/courses_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CoursesController, type: :controller do
+  describe "courses#index" do
+    it "should load the page"
+
+    it "should load all courses on the index page"
+  end
+
+  describe "courses#show" do
+    it "should load the page with a valid id"
+
+    it "should present an error with an invalid id"
+  end
+end
