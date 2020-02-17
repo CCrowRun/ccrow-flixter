7afdb5915b548ccc32a89af712f9defa214ae8b3
Carrie <carrie.g.crow@gmail.com>
2020-02-17 20:10:31 +0000

    Implemented test for dashboard show page

---
+++ spec/controllers/dashboards_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe DashboardsController, type: :controller do
+  describe "dashboard#show" do
+    let!(:user){ FactoryBot.create(:user) }
+
+    it "should show a user's dashboard" do
+      sign_in user
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  # For Devise > 4.1.1
+  config.include Devise::Test::ControllerHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
