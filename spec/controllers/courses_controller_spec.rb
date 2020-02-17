8ff3da06f672e733e37518d85410ddba88a39b4d
Carrie <carrie.g.crow@gmail.com>
2020-02-17 19:44:22 +0000

    Added test for course show page

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -7,12 +7,17 @@ RSpec.describe CoursesController, type: :controller do
 
       expect(response).to have_http_status(:success)
     end
-
-    it "should load all courses on the index page"
   end
 
   describe "courses#show" do
-    it "should load the page with a valid id"
+    let(:user) { FactoryBot.create(:user) }
+    it "should load the page with a valid id" do
+      course = FactoryBot.create(:course, user_id: user.id)
+
+      get :show, params: { id: course.id }
+
+      expect(response).to have_http_status(:success)
+    end
 
     it "should present an error with an invalid id"
   end
