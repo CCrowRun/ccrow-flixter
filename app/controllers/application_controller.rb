c08eaa87b645043823c828ed2458a2e9d54ef0c1
Carrie <carrie.g.crow@gmail.com>
2020-02-17 20:00:13 +0000

    Refactored not_found into application controller before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+private
+  def not_found
+    render plain: "#{status.to_s.titleize} invalid ID", status: :not_found
+  end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,16 @@
 class CoursesController < ApplicationController
+  before_action :load_course, only: [:show]
+  before_action :not_found, only: [:show], if: -> { @course.blank? }
+  
   def index
     @courses = Course.all
   end
 
   def show
+  end
+
+private
+  def load_course
     @course = Course.find_by(id: params[:id])
-    render plain: "#{status.to_s.titleize} :(", status: :not_found if @course.blank?
   end
 end
