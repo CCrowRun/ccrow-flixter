06834ead83d854688bb6a0baa0079bc25ed38831
Carrie <carrie.g.crow@gmail.com>
2020-02-25 19:23:36 +0000

    Created spec folder for courses under instructor namespace

---
+++ spec/controllers/instructor/courses_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'instructor/courses_controller.rb'
+
+RSpec.describe Instructor::CoursesController, type: :controller do
+  describe "courses#new" do
+    it "should load the page" 
+  end
+end
