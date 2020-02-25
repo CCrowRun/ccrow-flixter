25853885bf97982774adb5d78710623b0035c8d3
Carrie <carrie.g.crow@gmail.com>
2020-02-25 19:38:18 +0000

    Created pending tests for remaining instructor course actions

--- spec/controllers/instructor/courses_controller_spec.rb
+++ spec/controllers/instructor/courses_controller_spec.rb
@@ -3,13 +3,41 @@ require 'instructor/courses_controller.rb'
 
 RSpec.describe Instructor::CoursesController, type: :controller do
   context "instructor logged in" do
-    let(:user){ FactoryBot.create(:user) }
+    before :each do
+      user = FactoryBot.create(:user)
+      sign_in user
+    end
     describe "courses#new" do
       it "should load the page" do
-        sign_in user
         get :new
         expect(response).to have_http_status(:success)
       end
+      it "should generate a new course in the database"
+    end
+    describe "courses#create" do
+      it "should load the course creation page"
+
+      it "should save valid courses to the database"
+
+      it "should render unprocessable_entity for invalid courses"
+    end
+    describe "courses#show" do
+      it "should load the page for a valid id"
+
+      it "should not load a page with an invalid id"
+
+    end
+  end
+  context "instructor logged in" do
+    let(:user){ FactoryBot.create(:user) }
+    describe "courses#new" do
+      it "present an error if the user is not logged in" 
+    end
+    describe "courses#create" do
+      it "present an error if the user is not logged in" 
+    end
+    describe "courses#show" do
+      it "present an error if the user is not logged in" 
     end
   end
 end
