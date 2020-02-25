e34d7f52cf9cd47fbbc373ce54e3c1a5116d48d1
Carrie <carrie.g.crow@gmail.com>
2020-02-25 19:32:42 +0000

    Implemented courses new in instructor namespace

--- spec/controllers/instructor/courses_controller_spec.rb
+++ spec/controllers/instructor/courses_controller_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 require 'instructor/courses_controller.rb'
 
 RSpec.describe Instructor::CoursesController, type: :controller do
-  describe "courses#new" do
-    it "should load the page" 
+  context "instructor logged in" do
+    let(:user){ FactoryBot.create(:user) }
+    describe "courses#new" do
+      it "should load the page" do
+        sign_in user
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
