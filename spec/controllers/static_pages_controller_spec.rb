7a783485b07cc85b057cf025be0dcb49fd641150
Carrie <carrie.g.crow@gmail.com>
2020-02-25 19:15:09 +0000

    Implemented static pages controller tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,15 +2,27 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe "static_pages#index" do
-    it "should successfully load the index page"
+    it "should successfully load the index page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
   end
   describe "static_pages#privacy" do
-    it "should successfully load the privacy page"
+    it "should successfully load the privacy page" do
+      get :privacy
+      expect(response).to have_http_status(:success)
+    end
   end
   describe "static_pages#team" do
-    it "should successfully load the team page"
+    it "should successfully load the team page" do
+      get :team
+      expect(response).to have_http_status(:success)
+    end
   end
   describe "static_pages#careers" do
-    it "should successfully load the careers page"
+    it "should successfully load the careers page" do
+      get :careers
+      expect(response).to have_http_status(:success)
+    end
   end
 end
