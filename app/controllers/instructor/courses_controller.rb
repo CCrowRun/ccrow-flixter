149b8cdc7384583823f0f1291214e070acac10f2
Carrie <carrie.g.crow@gmail.com>
2020-02-25 20:22:12 +0000

    Refactored not_found to provide 404 error on invalid ID's

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,39 +1,39 @@
 class Instructor::CoursesController < ApplicationController
-    before_action :authenticate_user!
-    before_action :require_authorized_for_current_course, only: [:show], if: -> { current_course }
+  before_action :authenticate_user!
+  before_action :not_found, only: [:show], if: -> { current_course.blank? }
+  before_action :require_authorized_for_current_course, only: [:show]
 
-    def new
-      @course = Course.new
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def create
-      @course = current_user.courses.create(course_params)
-      if @course.valid?
-        redirect_to instructor_course_path(@course)
-      else
-        render :new, status: :unprocessable_entity
-      end
+  def show
+    @section = Section.new
+    @lesson = Lesson.new
+  end
+
+private
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
     end
+  end
 
-    def show
-      @section = Section.new
-      @lesson = Lesson.new
-    end
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find_by(id: params[:id])
+  end
 
-    private
-
-    def require_authorized_for_current_course
-      if current_course.user != current_user
-        render plain: "Unauthorized", status: :unauthorized
-      end
-    end
-
-    helper_method :current_course
-    def current_course
-      @current_course ||= Course.find_by(id: params[:id])
-    end
-
-    def course_params
-      params.require(:course).permit(:title, :description, :cost, :image)
-    end
+  def course_params
+    params.require(:course).permit(:title, :description, :cost, :image)
+  end
 end
