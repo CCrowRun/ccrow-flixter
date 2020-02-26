357f8099ad316c91c170ccef545a3f74be05c459
Carrie <carrie.g.crow@gmail.com>
2020-02-26 19:09:39 +0000

    Refactored instructor courses controller tests

--- spec/controllers/instructor/courses_controller_spec.rb
+++ spec/controllers/instructor/courses_controller_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 require 'instructor/courses_controller.rb'
 
 RSpec.describe Instructor::CoursesController, type: :controller do
-  context "instructor logged in" do
+  include_examples "curriculum"
+  context "authorized instructor is logged in" do
     before :each do
-      user = FactoryBot.create(:user)
       sign_in user
-      @test_course = FactoryBot.create(:course, user_id: user.id)
     end
     describe "courses#new" do
       it "should load the page" do
@@ -17,22 +16,22 @@ RSpec.describe Instructor::CoursesController, type: :controller do
     describe "courses#create" do
       it "should create courses with valid parameters" do
         post :create, params: { course: { title: "Test Title", description: "Controller test", cost: 0 } }
-        course = Course.last
-        expect(response).to redirect_to instructor_course_path(course)
-        expect(course.title).to eq "Test Title"
-        expect(course.description).to eq "Controller test"
-        expect(course.cost).to eq 0
+        test_course = Course.last
+        expect(response).to redirect_to instructor_course_path(test_course)
+        expect(test_course.title).to eq "Test Title"
+        expect(test_course.description).to eq "Controller test"
+        expect(test_course.cost).to eq 0
       end
       it "should render unprocessable_entity for invalid courses" do
         post :create, params: { course: { title: "Invalid Test Title" } }
         expect(response).to have_http_status(:unprocessable_entity)
-        course = Course.last
-        expect(course.title).not_to eq "Invalid Test Title"
+        test_course = Course.last
+        expect(test_course.title).not_to eq "Invalid Test Title"
       end
     end
     describe "courses#show" do
       it "should load the page for a valid id" do
-        get :show, params: { id: @test_course.id }
+        get :show, params: { id: course.id }
         expect(response).to have_http_status(:success)
       end
       it "should not load a page with an invalid id" do
@@ -41,11 +40,7 @@ RSpec.describe Instructor::CoursesController, type: :controller do
       end
     end
   end
-  context "instructor not logged in" do
-    before :each do
-      other_user = FactoryBot.create(:user)
-      @course = FactoryBot.create(:course, user_id: other_user.id)
-    end
+  context "user is not logged in" do
     describe "courses#new" do
       it "should redirect unauthenticated users to sign in" do
         get :new  
@@ -60,13 +55,17 @@ RSpec.describe Instructor::CoursesController, type: :controller do
     end
     describe "courses#show" do
       it "should redirect unauthenticated users to sign in" do
-        get :show, params: { id: @course.id }  
+        get :show, params: { id: course.id }  
         expect(response).to redirect_to new_user_session_path
       end
+    end
+  end
+  context "unauthorized instructor is logged in" do
+    describe "courses#show" do
       it "should present an error if the incorrect user is logged in" do
-        user = FactoryBot.create(:user)
-        sign_in user
-        get :show, params: {id: @course.id}
+        other_user = FactoryBot.create(:user)
+        sign_in other_user
+        get :show, params: {id: course.id}
         expect(response).to have_http_status(:unauthorized)
       end
     end
